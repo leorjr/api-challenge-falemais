4551af68b29279035be4b9ffc6019fac6e159e59
Leonardo Rodrigues <leonardorodrigues@Air-de-Leonardo.local>
2022-05-01 11:51:51 -0300

    feat: created logical for calculate values

--- app/controlles/home_controller.py
+++ app/controlles/home_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from app.utils import functions
 
 
@@ -21,3 +21,38 @@ def init_app(app: Flask):
         data = functions.load_db()
 
         return jsonify(data["tarifacao"])
+
+    @app.post("/calcular")
+    def calculate():
+
+        dados_request = request.json
+
+        required_keys = ["origem", "destino", "tempo", "plano"]
+
+        # TODO: refatorar aqui para que fique em uma função
+
+        for key in dados_request.keys():
+            if key not in required_keys:
+                return {"err": f"Informar apenas origem, destino, tempo e plano escolhido - chave {key} desconhecida"}
+
+        for key in required_keys:
+            if key not in dados_request.keys():
+                return {"err": f"favor informar {key}"}
+
+        db = functions.load_db()
+
+        tarifacao = {}
+        plano_escolhido = {}
+
+        for item in db["tarifacao"]:
+            if (item["origem"] == dados_request["origem"]) and (item["destino"] == dados_request["destino"]):
+                tarifacao = item
+
+        for item in db["planos"]:
+            if item["nome"] == dados_request["plano"]:
+                plano_escolhido = item
+
+        response = functions.calculate(
+            plano_escolhido, tarifacao, dados_request)
+
+        return response
--- app/utils/functions.py
+++ app/utils/functions.py
@@ -6,3 +6,24 @@ def load_db():
         data = json.load(file)
 
     return data
+
+
+def calculate(plano_escolhido, tarifacao, dados_request):
+
+    saldo_restante = plano_escolhido["minutos"] - dados_request["tempo"]
+
+    valor_a_pagar = 0 if saldo_restante > 0 else ((
+        tarifacao["minuto"] + (tarifacao["minuto"] * 0.10)) * saldo_restante) * (-1)
+
+    valor_a_pagar_sem_plano = dados_request["tempo"] * tarifacao["minuto"]
+
+    calculated = {
+        "origem": tarifacao["origem"],
+        "destino": tarifacao["destino"],
+        "tempo": dados_request["tempo"],
+        "plano FaleMais": dados_request["plano"],
+        "com FaleMais": valor_a_pagar,
+        "sem FaleMais": valor_a_pagar_sem_plano,
+    }
+
+    return calculated
